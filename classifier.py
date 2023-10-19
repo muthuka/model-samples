ac148a5b44b2233706bc38dd37dafe5e40a3fbb5
Muthu <muthu.kumar@me.com>
2023-10-18 21:47:36 -0400

    Initial version

---
+++ classifier.py
@@ -0,0 +1,11 @@
+import torch
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
+
+model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier = pipeline("sentiment-analysis", model=model, tokenizer=tokenizer)
+result = classifier(
+    "Nous sommes très heureux de vous présenter la bibliothèque 🤗 Transformers.")
+print(result)
